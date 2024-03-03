8cdd2b39421c05140190705aaeb9e88458e46cce
sarapinit <sarapinit@ya.ru>
2024-03-03 23:24:58 +0700

    nvim: merge options from old kickstart config to current config

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,11 +1,22 @@
 vim.opt.cursorline = true
-vim.opt.guicursor = 'a:blinkon1'
-vim.opt.scrolloff = math.floor(0.5 * vim.o.lines)
+vim.opt.scrolloff = 999
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.signcolumn = 'yes'
+vim.opt.mouse = 'a'
+
+-- NOTE: You should make sure your terminal supports this
+vim.opt.termguicolors = true
+
+--  Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.opt.breakindent = true
 
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
@@ -21,9 +32,17 @@ vim.opt.smartcase = true
 
 vim.opt.showmode = false
 
+-- Set completeopt to have a better completion experience
+vim.opt.completeopt = 'menuone,noselect'
+
 vim.opt.backup = false
 vim.opt.swapfile = false
 vim.opt.undodir = os.getenv('HOMEPATH') .. '/.nvim/undodir'
 vim.opt.undofile = true
 
+-- Decrease update time
 vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+vim.opt.foldmethod = "expr"
+-- vim.o.foldexpr = "nvim_treesitter#foldexpr()"
