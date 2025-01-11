7f5c46ed48a440f9b1d79476c1a4f6b31ef2628c
sarapinit <sarapinit@ya.ru>
2025-01-11 22:08:57 +0700

    rewrite powershell setup

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -45,19 +45,13 @@ vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
 vim.o.foldmethod = "expr"
--- vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 
-vim.cmd [[
-    set shellcmdflag=-command
-    set shellquote=\"
-    set shellxquote=
-    set shell=powershell
-]]
-
--- local sysname = string.lower(vim.loop.os_uname().sysname)
--- local is_windows = string.match(sysname, "windows")
---
--- if is_windows then
---     vim.o.shell = "pwsh.exe"
---     vim.g.shell = "pwsh.exe"
--- end
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+    vim.cmd [[
+            let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+            let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+            let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+            let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+            set shellquote= shellxquote=
+    ]]
+end
